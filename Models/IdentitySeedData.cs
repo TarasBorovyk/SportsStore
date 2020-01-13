714e4192693792e3d47ed2422e30c68981196607
Tapacuk <ken4ik.gamer@gmail.com>
2020-01-13 13:18:37 +0200

    Initial commit

---
+++ Models/IdentitySeedData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportsStore.Models
+{
+    public static class IdentitySeedData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "Secret123$";
+
+        public static async void EnsurePopulated(IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                //Resolve ASP .NET Core Identity with DI help
+                var userManager = (UserManager<IdentityUser>)scope.ServiceProvider.GetService(typeof(UserManager<IdentityUser>));
+                IdentityUser user = await userManager.FindByIdAsync(adminUser);
+                if(user == null)
+                {
+                    user = new IdentityUser("Admin");
+                    await userManager.CreateAsync(user, adminPassword);
+                }
+            }           
+        }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+                if (!context.Products.Any())
+                {
+                    context.Products.AddRange(
+                        new Product
+                        {
+                            Name = "Kayak",
+                            Description = "A boat foa one person",
+                            Category = "Watersports",
+                            Price = 275
+                        },
+                        new Product
+                        {
+                            Name = "Lifejacket",
+                            Description = "Protective and fashionable",
+                            Category = "Watersports",
+                            Price = 48.95m
+                        },
+                        new Product
+                        {
+                            Name = "Soccer ball",
+                            Description = "FIFA-approved size and weight",
+                            Category = "Soccer",
+                            Price = 19.50m
+                        },
+                        new Product
+                        {
+                            Name = "Corner Flags",
+                            Description = "Give your playing field a professional touch",
+                            Category = "Soccer",
+                            Price = 34.95m
+                        },
+                        new Product
+                        {
+                            Name = "Stadium",
+                            Description = "Fast-packed 35,000-seat stadium",
+                            Category = "Soccer",
+                            Price = 79500
+                        },
+                        new Product
+                        {
+                            Name = "Thinking cap",
+                            Description = "Improve brain efficiency by 75%",
+                            Category = "Chess",
+                            Price = 16
+                        },
+                        new Product
+                        {
+                            Name = "Unsteady chair",
+                            Description = "Secretly give your opponent disadvantage",
+                            Category = "Chess",
+                            Price = 29.95m
+                        },
+                        new Product
+                        {
+                            Name = "Human Chess Board",
+                            Description = "A fun game for the family",
+                            Category = "Chess",
+                            Price = 75
+                        },
+                        new Product
+                        {
+                            Name = "Bling-Bling King",
+                            Description = "Gold-plated, diamond-studded King",
+                            Category = "Chess",
+                            Price = 1200
+                        }
+                    );
+                }
+            //ApplicationDbContext context = app.ApplicationServices.GetRequiredService<ApplicationDbContext>();
+                context.SaveChanges();
+            }
+        }
+    }
+}
